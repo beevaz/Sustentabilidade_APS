8e862caf3f328369f002e06384e50a4b22f852a4
Beatriz Fialho <beatriz_fialho@hotmail.com>
2019-10-30 23:03:43 -0300

    APS DSD Sustentabilidade

--- sustentavel-aps-meio/src/main/java/br/com/sustentavel/repository/ArvoreRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sustentavel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.sustentavel.entity.ArvoreModel;
-
-@Repository
-public interface ArvoreRepository extends JpaRepository<ArvoreModel, Long> { }
--- sustentavel-aps-meio/src/main/java/br/com/sustentavel/repository/OleoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sustentavel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.sustentavel.entity.OleoModel;
-
-@Repository
-public interface OleoRepository extends JpaRepository<OleoModel, Long> { }
